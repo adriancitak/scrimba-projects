083b29006f4e1b57eda8b201096ff0b7eae99d75
adriancitak <75241775+adriancitak@users.noreply.github.com>
2025-04-23 17:45:06 +0300

    Added New Game button

--- basketball-score/main.js
+++ basketball-score/main.js
@@ -17,35 +17,71 @@ function addThree (num) {
     return num + 3;
 }
 
+function checkWinner(){
+    const homeBox = document.querySelector('.home-box');
+    const guestBox = document.querySelector('.guest-box');
+
+    homeBox.classList.remove('flash-winner');
+    guestBox.classList.remove('flash-winner');
+
+    if (home > guest){
+        homeBox.classList.add('flash-winner');
+    } else if (guest > home){
+        guestBox.classList.add('flash-winner')
+    }
+}
+
+
 document.querySelector('#home-1').addEventListener('click', () => {
     home = addOne(home);
     homeScore.textContent = home;
+    checkWinner()
 })
 
 document.querySelector('#home-2').addEventListener('click', () => {
     home = addTwo(home);
     homeScore.textContent = home;
+    checkWinner()
+
 })
 
 document.querySelector('#home-3').addEventListener('click', () => {
     home = addThree(home);
     homeScore.textContent = home;
+    checkWinner()
+
 })
 
 
 document.querySelector('#guest-1').addEventListener('click', () => {
     guest = addOne(guest);
     guestScore.textContent = guest;
+    checkWinner()
+
 })
 
 document.querySelector('#guest-2').addEventListener('click', () => {
     guest = addTwo(guest);
     guestScore.textContent = guest;
+    checkWinner()
+
 })
 
 document.querySelector('#guest-3').addEventListener('click', () => {
     guest = addThree(guest);
     guestScore.textContent = guest;
+    checkWinner()
+
 })
+
+document.querySelector('.new-game').addEventListener('click', () => {
+    guest = 0;
+    home = 0;
+    guestScore.textContent = guest;
+    homeScore.textContent = home;
+    checkWinner()
+})
+
+
 
 
