3aa916d2c5cc2ed4ce7ea77022ccdf78c0f0f7f2
adriancitak <75241775+adriancitak@users.noreply.github.com>
2025-04-26 18:35:54 +0300

    Adjust input font-size

--- unit-converter/main.js
+++ unit-converter/main.js
@@ -3,6 +3,22 @@ const lengthEl = document.querySelector('#length');
 const volumeEl = document.querySelector('#volume');
 const massEl = document.querySelector('#mass') 
 
+const input = document.querySelector('input');
+
+input.addEventListener('input', () => {
+    const length = input.value.length;
+
+    if (length <= 3){
+        input.style.fontSize = '4rem';
+    } else if (length <= 4){
+        input.style.fontSize = '3rem';
+    } else if (length <= 5){
+        input.style.fontSize = '2rem';
+    } else {
+        input.style.fontSize = '1.5rem'
+    }
+})
+
 
 function meterToFeet(num){
     return (num * 3.28084).toFixed(3);
@@ -34,14 +50,7 @@ btn.addEventListener('click', () => {
     const volume = `${num} liters = ${literToGallon(num)} gallons | ${num} gallons = ${gallonToLiter(num)} liters`
     const mass = `${num} kilos = ${kiloToPound(num)} pounds | ${num} pounds = ${poundToKilo(num)} kilos`
 
-
-
-
-
     lengthEl.textContent = length;
     volumeEl.textContent = volume;
-    massEl.textContent = mass;
-    
-
-
+    massEl.textContent = mass;    
 })
