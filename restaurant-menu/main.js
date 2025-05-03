b9a4fea8f4ec4aac33a6e076568b2acc1bebe589
adriancitak <75241775+adriancitak@users.noreply.github.com>
2025-05-03 17:38:59 +0300

    Added the initial styling

--- restaurant-menu/main.js
+++ restaurant-menu/main.js
@@ -1,2 +1,29 @@
 import { menuArray } from "/data.js"
 
+
+const itemsEl = document.querySelector('.items');
+
+function render (){
+
+    const itemsHTML = menuArray.map(food => {
+        return `
+            <div class="food-card">
+                <div class="card-left">
+                    <span class="food-emoji">${food.emoji}</span>
+                    <div class="food-info">
+                        <p class='food-title'>${food.name}</p>
+                        <p class='ingredients'>${food.ingredients.join(' ')}</p>
+                        <p class="price">$${food.price}</p>
+                    </div>
+                </div>
+                <div class="card-right">
+                    <button class="atc-btn"><p>+</p></button>
+                </div>
+            </div>
+        `
+    })
+
+    itemsEl.innerHTML = itemsHTML.join('');
+}
+
+render()
