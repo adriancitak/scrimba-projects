7bb046d5459a6bab13f261dd07388a152e61cbe6
adriancitak <75241775+adriancitak@users.noreply.github.com>
2025-05-04 12:42:38 +0300

    Added add to cart function

--- restaurant-menu/main.js
+++ restaurant-menu/main.js
@@ -2,6 +2,14 @@ import { menuArray } from "/data.js"
 
 
 const itemsEl = document.querySelector('.items');
+const checkoutEl = document.querySelector('.checkout')
+
+// check if anything is added to cart isAdded = bool
+// if added render the checkout 
+//make checkout clickable
+//once clicked make a payment card with pay btn
+//after payment show success
+
 
 function render (){
 
@@ -17,13 +25,72 @@ function render (){
                     </div>
                 </div>
                 <div class="card-right">
-                    <button class="atc-btn"><p>+</p></button>
+                    <button class="atc-btn" data-id="${food.id}"><p>+</p></button>
                 </div>
             </div>
         `
     })
 
+    
+
     itemsEl.innerHTML = itemsHTML.join('');
 }
 
 render()
+
+const order = [];
+
+itemsEl.addEventListener('click', (e) => {
+    const btn = e.target.closest('.atc-btn')
+    if (btn) {
+        const itemId = parseInt(btn.dataset.id);
+        const item = menuArray.find(food => food.id === itemId);
+        order.push(item)
+        renderCheckout(order)
+    }
+})
+
+
+function renderCheckout(order){
+
+    if (!order){
+        checkoutEl.innerHTML = ''
+    } else {
+        const itemHTML = order.map(food => {
+            return `
+
+            <div class="food-checkout>
+                <div class = "left-order">
+                    <p class = "food-checkout-name">${food.name}
+                    <button class = "remove">remove</button 
+                </div>
+                <div class = "right-order">
+                    <p class="order-price">${food.price}</p>
+                </div>
+            </div>
+            
+            
+            
+            `
+        }).join('')
+
+        const totalPrice = order.reduce((acc, num) => acc + num.price, 0);
+        console.log(totalPrice)
+    
+        const checkoutHTML = `
+        
+        <div class="order-sec>
+            <h3>Your order</h3>
+            ${itemHTML}
+            <div class = "price">
+                <p>Total price:</p>
+                <p>${totalPrice}</p>       
+            </div>`
+
+        checkoutEl.innerHTML = checkoutHTML;
+    }
+
+
+    
+
+}
